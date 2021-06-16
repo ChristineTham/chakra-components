73d50b74335bd68021da2a75f9773db785ab8f82
Christine Tham <chris.tham@hellotham.com>
2021-06-16 22:33:11 +1000

    Upgrade to NextJS 11 + latest, removed prettier

--- src/components/sections/SectionHead.tsx
+++ src/components/sections/SectionHead.tsx
@@ -29,6 +29,7 @@ export const AboutMe: React.FC = () => {
             w={{ lg: 1 / 2 }}
             fit="cover"
             align="center"
+            alt="About Me cover"
             src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"
             rounded="lg"
           />
